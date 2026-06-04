0b70f76e847ece3d021baf9c3ac61af6474bb150
hollyursule_ <hollykalisa@gmail.com>
2026-06-04 21:25:08 +0200

    Switch Logo to flavour_nest_logo.svg and remove placeholder

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { RouterLink } from 'vue-router'
-import logoUrl from '../assets/flavor_nest_logi.svg'
+import logoUrl from './flavour_nest_logo.svg'
 </script>
 
 <template>
   <RouterLink to="/" class="site-logo">
     <span class="site-mark" aria-hidden="true">
-      <img :src="logoUrl" alt="Flavor Nest logo" width="48" height="48" />
+      <img :src="logoUrl" alt="Flavour Nest logo" width="48" height="48" />
     </span>
     <span>
       <strong>Flavour Nest</strong>
